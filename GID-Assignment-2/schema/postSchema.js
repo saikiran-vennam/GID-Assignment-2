c7cf3c1a34c3802b4e194a3e1aaf0aa11bd74744
saikiran-vennam <107356058+saikiran-vennam@users.noreply.github.com>
2022-09-12 10:13:41 +0530

    Add files via upload
    Added all files

---
+++ GID-Assignment-2/schema/postSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title : {
+        type :String, 
+        required : true
+    },
+    body : {
+        type: String, 
+        required : true
+    },
+    image: {
+        type: String, 
+        required: true
+    },
+    user : {
+        type: Schema.Types.ObjectId, 
+        ref: "User"
+    }
+})
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
